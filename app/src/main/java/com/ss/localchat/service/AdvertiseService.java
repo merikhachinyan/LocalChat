0fd454ca8c2c3fc9d59c2fb92f7b7f9bc5c330e6
merikh <merikh@github.com>
2018-08-14 02:33:25 +0400

    Added Settings Activity

--- app/src/main/java/com/ss/localchat/service/AdvertiseService.java
+++ app/src/main/java/com/ss/localchat/service/AdvertiseService.java
@@ -1,15 +1,22 @@
 package com.ss.localchat.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.ss.localchat.R;
+import com.ss.localchat.activity.SettingsActivity;
 
 public class AdvertiseService extends BaseService {
 
     public static final String CHANNEL_ID = "advertise_service";
+    public static final String NOTIFICATION_TITLE = "Local Chat";
+    public static final String NOTIFICATION_CONTENT = "Advertising...";
 
     private AdvertiseBinder mAdvertiseBinder;
 
@@ -50,16 +57,28 @@ public class AdvertiseService extends BaseService {
     }
 
     private void advertising() {
-        mConnectionsClient.startAdvertising("User", getPackageName(),
+        mConnectionsClient.startAdvertising("Name", getPackageName(),
                 mConnectionLifecycleCallback, new AdvertisingOptions.Builder()
                         .setStrategy(STRATEGY)
                         .build());
     }
 
-
     public void startForegroundAdvertiseService(){
         createNotificationChannel(CHANNEL_ID);
-        startForeground(1, createNotification("Local chat", "Advertising..."));
+        startForeground(1, createNotification(NOTIFICATION_TITLE, NOTIFICATION_CONTENT));
+    }
+
+    protected Notification createNotification(String title, String message){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        return builder.build();
     }
 
     public class AdvertiseBinder extends Binder {
